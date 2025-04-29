573117738b7dbcc1d9c8b9cd3b742d3e385b0f25
Musta <donmezmustafa125@gmail.com>
2025-04-28 21:56:15 -0400

    terraform init, val, plan, and apply were succesful

--- ledgerpay/ledgerpay/settings.py
+++ ledgerpay/ledgerpay/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-21u=#_b*t^pr&iz@l$s+4_n#mr0n2&avzl$&_0lr+^#xflljfm'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'unsafe-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.elasticbeanstalk.com'
+]
 
 
 # Application definition
@@ -119,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/payments/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "payments/static",  # The location of the static directory
 ]
---
+++ ledgerpay/terraform/provider.tf
@@ -0,0 +1,3 @@
+provider "aws" {
+  region = "us-east-2" # used this region because it was the initial aws region when I made my account
+}
