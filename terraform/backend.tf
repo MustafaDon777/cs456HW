b7d4d33d5bd942f3cdc224736bd3a44dfe5b379a
M Don <116388196+MustafaDon777@users.noreply.github.com>
2025-05-07 21:04:08 -0400

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-djangoapp-ledgerpay-mustafadon777" 
-    key    = "terraform.tfstate"
-    region = "us-east-2" 
-  }
-}
--- terraform/provider.tf
+++
@@ -1,3 +0,0 @@
-provider "aws" {
-  region = "us-east-2" # used this region because it was the initial aws region when I made my account
-}
